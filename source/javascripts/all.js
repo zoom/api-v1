c14c2ce032e4c9fee1425fc8602a40f64bfe9ff8
Joshua Woodward <joshua.woodward@zoom.us>
2017-06-13 13:47:38 -0700

    simplify sticky header

--- source/javascripts/all.js
+++ source/javascripts/all.js
@@ -3,46 +3,14 @@
 //= require ./app/_search
 //= require ./app/_toc
 
-var stick;
-
 jQuery(document).ready(function(){
-
-    stick = (function($) {
-        return function () {
-
-
-            var stickys = [];
-
-            var headers = $('.content h1, .content h2');
-            headers.each(function(a,b){
-                stickys[a] = {
-                    element: b,
-                    offset: (b.offsetTop),
-                    set: false,
-                    text: $(b).text()
-                };
-            });
-
-            $(window).scroll(function () {
-
-                if ($(window).scrollTop() == 0) {
-                    $('#sticky-header').hide();
-                    return false;
-                }
-
-                stickys.forEach(function(a){
-                    if ($(window).scrollTop() > a.offset) {
-                        $('#sticky-header').text(a.text);
-                        $('#sticky-header').show();
-                    }
-                });
-
-            });
+    $(window).scroll(function () {
+        if ($(window).scrollTop() == 0) {
+            $('#sticky-header').hide();
+            return false;
         }
 
-    })(jQuery);
-
-    stick();
-
-    jQuery(window).resize(stick);
+        $('#sticky-header').text(jQuery(window.location.hash).text());
+        $('#sticky-header').show();
+    });
 });
